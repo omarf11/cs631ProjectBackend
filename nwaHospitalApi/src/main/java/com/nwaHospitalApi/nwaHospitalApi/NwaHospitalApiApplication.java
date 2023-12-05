6b006ab76d7035a15c45803f539225b368b88606
omarf11 <ofaheem11@gmail.com>
2023-12-04 22:41:55 -0500

    init

---
+++ nwaHospitalApi/src/main/java/com/nwaHospitalApi/nwaHospitalApi/NwaHospitalApiApplication.java
@@ -0,0 +1,13 @@
+package com.nwaHospitalApi.nwaHospitalApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwaHospitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwaHospitalApiApplication.class, args);
+	}
+
+}
---
+++ nwaHospitalApi/src/test/java/com/nwaHospitalApi/nwaHospitalApi/NwaHospitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwaHospitalApi.nwaHospitalApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwaHospitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
