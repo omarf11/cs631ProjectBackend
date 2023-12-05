26666f048527c25f5204f35b950b6f1042cb08a0
omarf11 <ofaheem11@gmail.com>
2023-12-04 23:00:27 -0500

    employee entitiy

---
+++ nwaHospitalApi/src/main/java/com/nwaHospitalApi/nwaHospitalApi/Entities/Employee.java
@@ -0,0 +1,29 @@
+package com.nwaHospitalApi.nwaHospitalApi.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+    
+    @Id
+    private Long empId;
+
+    private Integer ssn;
+
+    private String address;
+
+    private Integer salary;
+
+    private String name;
+
+    private String gender;
+
+    private Integer phoneNumber;
+
+    private String type; 
+
+
+}
