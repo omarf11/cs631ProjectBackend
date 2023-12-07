ae92a0de976e507cf7db84a47b5eaa5b7f8f3db7
omarf11 <ofaheem11@gmail.com>
2023-12-07 14:37:33 -0500

    added cors config

--- nwaHospitalApi/src/main/java/com/nwaHospitalApi/nwaHospitalApi/Controllers/EmployeeController.java
+++ nwaHospitalApi/src/main/java/com/nwaHospitalApi/nwaHospitalApi/Controllers/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.nwaHospitalApi.nwaHospitalApi.Services.EmployeeService;
 @RestController
 @Controller
 @RequestMapping("/employees")
+@CrossOrigin(origins = "http://localhost:3000") 
 public class EmployeeController {
 
    private final EmployeeService employeeService;
