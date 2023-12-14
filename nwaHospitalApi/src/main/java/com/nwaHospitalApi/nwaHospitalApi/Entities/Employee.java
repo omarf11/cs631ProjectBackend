c26f84a02f87cd304893947d3d1892eed85f6440
omarf11 <ofaheem11@gmail.com>
2023-12-14 15:03:33 -0500

    fixed creation bug

--- nwaHospitalApi/src/main/java/com/nwaHospitalApi/nwaHospitalApi/Entities/Employee.java
+++ nwaHospitalApi/src/main/java/com/nwaHospitalApi/nwaHospitalApi/Entities/Employee.java
@@ -1,6 +1,8 @@
 package com.nwaHospitalApi.nwaHospitalApi.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -11,6 +13,7 @@ import lombok.Data;
 public class Employee {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer emp_id;
 
     private Integer ssn;
